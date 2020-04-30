64f9186b1335bb5ac3a5aadcdfa544700262dd7c
guangxing <1042445773@qq.com>
2020-04-30 16:10:04 +0800

    增加描述文字

--- src/main/java/com/guangxing/cache/SpringBootCacheApplication.java
+++ src/main/java/com/guangxing/cache/SpringBootCacheApplication.java
@@ -7,17 +7,23 @@ import org.springframework.cache.annotation.EnableCaching;
 
 
 /**
- * 搭建基本环境
+ * 一、搭建基本环境
  * 1、导入数据库文件
  * 2、创建javaBean封装对象
  * 3、整合Mybatis操作数据库
  *      1、配置数据源信息
  *      2、使用注解版的mybatis
  *          1）、@MapperScan指定需要扫描的mapper接口所在的包
- * 4、快速开启缓存
+ *二、快速开启缓存
  *  步骤：
  *      1、开启基于注解的缓存
  *      2、标注缓存注解即可
+ *
+ * 默认使用的是ConcurrentMapCacheManage>ConcurrentMapCache;将数据保存在ConcurrentMap<Object,Object>
+ *     开发中使用缓存中间件：redis、memcached、ehcache
+ *
+ *三、整合Redis作为缓存
+ * Redis是一个开源的，内存中的数据存储系统，他可以用作数据库、缓存和消息中间件
  *
  */
 @SpringBootApplication
