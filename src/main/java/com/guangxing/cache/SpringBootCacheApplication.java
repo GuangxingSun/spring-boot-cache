b07154c212237b5fa7f0e53bdb3f943f8f345cb3
guangxing <1042445773@qq.com>
2020-05-08 17:43:53 +0800

    增加redis说明

--- src/main/java/com/guangxing/cache/SpringBootCacheApplication.java
+++ src/main/java/com/guangxing/cache/SpringBootCacheApplication.java
@@ -29,6 +29,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  *
  *三、整合Redis作为缓存
  * Redis是一个开源的，内存中的数据存储系统，他可以用作数据库、缓存和消息中间件
+ *  1、安装redis：使用docker
+ *  2、引入redis的starter
+ *  3、配置redis
+ *  4、测试缓存
+ *      原理：CacheManger==Cache缓存组件实际给缓存中存取数据
+ *      1）引入redis的starter，容器中保存的是RedisCacheManager；
+ *      2）redisCacheManager帮我们创建RedisCache来作为缓存组件；RedisCache通过操作redis缓存数据。
+ *
  *
  *
  *
